616449e444a0c325765313302d1f9480ed49bc35
rpanic <raphael.panic@gmx.at>
2018-08-22 21:36:06 +0200

    Integrated Retrieving of missing Transactions over the network Restructured the Transaction Queue a bit

--- src/Main/TransactionQueue.java
+++ src/Main/TransactionQueue.java
@@ -4,27 +4,57 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import model.Hash;
 import newMain.Transaction;
 
 public class TransactionQueue {
-
-	//TODO Use it in Code
 	
 	HashMap<Transaction, List<Hash>> map = new HashMap<>();
-	HashMap<Transaction, List<Predicate<Transaction>>> actions = new HashMap<>();
+	//HashMap<Transaction, List<Predicate<Transaction>>> actions = new HashMap<>();
+	Predicate<Hash> retrieve;
+	Predicate<Transaction> action;
 	
-	public void add(Transaction t, List<Hash> conflict, Predicate<Transaction> action) {
+	Thread retrievingThread;
+	
+	public TransactionQueue(Predicate<Transaction> action, Predicate<Hash> retrieve){
+		this.retrieve = retrieve;
+		this.action = action;
+		
+		this.retrievingThread = new Thread(() -> {
+			
+			while(true){
+
+				try {
+					Thread.sleep(1000L);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				map.values().stream().flatMap(List::stream).forEach(x -> retrieveTransaction(x));
+				
+			}
+			
+		}, "TransactionQueue RetrievingThread");
+		retrievingThread.start();
+	}
+	
+	public void add(Transaction t, List<Hash> conflict) {
 		
 		if(map.containsKey(t)) {
 			map.get(t).addAll(conflict);
-			
 		}else{
 			List<Hash> list = new ArrayList<>();
 			list.addAll(conflict);
 			map.put(t, list);
 		}
+		
+	}
+	
+	public void retrieveTransaction(Hash t){
+		
+		retrieve.test(t);
 		
 	}
 	
@@ -43,8 +73,9 @@ public class TransactionQueue {
 			if((i = conflicts.indexOf(t.getTxId())) >= 0) {
 				
 				if(conflicts.size() == 1) {
-					Predicate<Transaction> predicate = actions.get(candidate).get(i);
-					boolean success = predicate.test(candidate);
+					//Predicate<Transaction> predicate = actions.get(candidate).get(i);
+					
+					boolean success = action.test(candidate);
 					if(success) {
 						map.remove(candidate);
 					}
